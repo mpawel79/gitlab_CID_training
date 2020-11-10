477fc5449c71da83b26f32c0a165cc4499a1db03
Pawel M <pivko.vodko@gmailcom>
2020-11-10 20:48:58 +0100

    Added onboarding.py

---
+++ onboarding.py
@@ -0,0 +1,7 @@
+names = ['Bob', 'Julian', 'Rick', 'Fran', 'Murph', 'Jenkins', 'Leroy']
+
+print('Onboarding...')
+for name in names:
+   print(name + ' is saying hello!')
+
+print('Onboarding done!')
