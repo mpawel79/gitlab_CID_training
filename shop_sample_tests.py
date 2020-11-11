803131c8d852e563d28ed1fbeca5c7d6c3e81fa4
Pawel M <pivko.vodko@gmailcom>
2020-11-11 01:15:04 +0100

    Mock self.mock_actual_value removed. Chrome start - with ui for 'Darwin' systems - headless for rest, base on PATH

--- shop_sample_tests.py
+++ shop_sample_tests.py
@@ -1,22 +1,38 @@
 import unittest
 
+import  platform
+from selenium import webdriver
 
 class ShopSampleTest(unittest.TestCase):
 
     def setUp(self):
         print("setUp")
 
-        self.mock_actual_value = 'Lost Hat'
+        if platform.uname()[0] == 'Darwin':
+            print("Darwin - local")
+            self.driver = webdriver.Chrome(executable_path=r'libs/chromedriver')
 
+        else:
 
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--disable-gpu')
+
+            #self.driver = webdriver.Chrome(options=chrome_options)
+            self.driver = webdriver.Chrome(options=chrome_options)
+
+        self.base_url = 'https://autodemo.testoneo.com/en/'
 
     def tearDown(self):
         print('tearDown')
+        self.driver.quit()
 
     def testMainTitle(self):
 
-        expected_value = 'Lost Hat'
+        self.driver.get(self.base_url)
 
-        actual_value = self.mock_actual_value
+        expected_value = 'Lost Hat'
+        actual_value = self.driver.title
 
         self.assertEqual(expected_value, actual_value)
