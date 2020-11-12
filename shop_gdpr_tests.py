c52e973efedc56aa0c3816fe74abe21b890b8fa9
Pawel M <pivko.vodko@gmailcom>
2020-11-12 01:05:42 +0100

    Added ui check of contact us page.

---
+++ shop_gdpr_tests.py
@@ -0,0 +1,39 @@
+import unittest
+
+import  platform
+from selenium import webdriver
+
+class ShopSampleTest(unittest.TestCase):
+
+    def setUp(self):
+        print("setUp")
+
+        if platform.uname()[0] == 'Darwin':
+            print("Darwin - local")
+            self.driver = webdriver.Chrome(executable_path=r'libs/chromedriver')
+
+        else:
+
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--disable-gpu')
+
+            #self.driver = webdriver.Chrome(options=chrome_options)
+            self.driver = webdriver.Chrome(options=chrome_options)
+
+        self.base_url = 'https://autodemo.testoneo.com/en/'
+
+    def tearDown(self):
+        print('tearDown')
+        self.driver.quit()
+
+    def testMainTitle(self):
+
+        contact_us_page = 'https://autodemo.testoneo.com/en/contact-us'
+        self.driver.get(contact_us_page)
+
+        expected_value = 'Contact us'
+        actual_value = self.driver.title
+
+        self.assertEqual(expected_value, actual_value)
