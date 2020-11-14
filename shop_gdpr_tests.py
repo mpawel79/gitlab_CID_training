7ba5369657f764eda2de0e426d9f10d56a758fbe
Pawel M <pivko.vodko@gmailcom>
2020-11-14 14:14:09 +0100

    Add Send and Invalid msg check.

--- shop_gdpr_tests.py
+++ shop_gdpr_tests.py
@@ -37,3 +37,11 @@ class ShopSampleTest(unittest.TestCase):
         actual_value = self.driver.title
 
         self.assertEqual(expected_value, actual_value)
+
+    def test_contact_us_invalid_email_error_for_empty_email(self):
+        contact_us_page = 'https://autodemo.testoneo.com/en/contact-us'
+        expected_title = 'Invalid email address.'
+        self.driver.get(contact_us_page)
+        self.driver.find_element_by_css_selector('.form-footer > .btn').click()
+        error = self.driver.find_element_by_css_selector('.alert.alert-danger > ul > li')
+        self.assertEqual(expected_title, error.text)
